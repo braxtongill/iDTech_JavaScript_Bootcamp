495e296c9ea507d4e78ecaddbe3c92a885fc1731
bgill <63630363+braxtongill@users.noreply.github.com>
2022-01-29 09:38:53 -0800

    uploaded initial commit

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,29 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let city = "Vancouver";
+let mysterious_creatures = "beavers";
+let man = "Johnathan";
+let address = "IHOP";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let creature_quantity = 5;
+let wake_up_time = 8;
+let hours_slept = 45;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let breakfast_food = ['juice','pancakes','waffles','french toast','cereal','plastic'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let full = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log("After my extremely restful " + hours_slept + " hours of sleep, I woke up at " + wake_up_time + " to start the day.");
+console.log("I decided to have a delicious breakfast of " + breakfast_food[2] + ".");
+console.log("However, I asked myself if I was full or not. The answer was " + full + ", so I decided to venture out into the wilderness of " + city + " for more food.");
+console.log("After fighting off " + creature_quantity + " " + mysterious_creatures + ", I finally arrived at a place I could get breakfast: " + address + ".");
+console.log("I talked to the owner, " + man + ", and I was able to enjoy a second breakfast of " + breakfast_food[1] + ". Delicious!");
